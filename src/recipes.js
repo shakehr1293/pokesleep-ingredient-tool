53c7e0a3d233e116a6255e8bdfee244cf08f93ed
shakehr1293 <doraharuto0902@gmail.com>
2025-12-12 12:44:01 +0900

    レシピデータ（仮）を追加

---
+++ src/recipes.js
@@ -0,0 +1,34 @@
+const recipes = [
+    {
+        name: "recipe1",
+        category: "カレー",
+        ingredients: [
+            {name: "ingredient1", count: 10},
+            {name: "ingredient2", count: 5},
+        ],
+    },
+    {
+        name: "recipe2",
+        category: "カレー",
+        ingredients: [
+            {name: "ingredient3", count: 8},
+            {name: "ingredient4", count: 10},
+        ],
+    },
+    {
+        name: "recipe3",
+        category: "サラダ",
+        ingredients: [
+            {name: "ingredient2", count: 12},
+            {name: "ingredient3", count: 15},
+        ],
+    },
+    {
+        name: "recipe4",
+        category: "デザート",
+        ingredients: [
+            {name: "ingredient1", count: 18},
+            {name: "ingredient4", count: 21},
+        ],
+    },
+];
